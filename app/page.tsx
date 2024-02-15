44c34bacf467fde9115bdc6fe8f4a32ff9d77b50
Jad <jazencode@gmail.com>
2024-02-15 20:36:17 +0800

    change size

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,20 @@ export default function Home() {
   return (
     <Grid h="100vh" w="100%" placeContent="center" bg="#EBF2F1">
       <Stack align="center">
-        <Image src="/logo.png" alt="Logo" width={500} height={500} />
-        <Text fontWeight="semibold" opacity={0.8} color="#04364A">
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          width={500}
+          height={500}
+          style={{ width: "15rem" }}
+        />
+        <Text
+          position="absolute"
+          bottom="2rem"
+          fontWeight="semibold"
+          opacity={0.8}
+          color="#04364A"
+        >
           Coming Soon
         </Text>
       </Stack>
